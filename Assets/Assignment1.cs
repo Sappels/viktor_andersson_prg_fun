aa5228edb2e2f746b021642d2385f3971cdaa490
Sappels <sappels66@gmail.com>
2021-09-13 10:10:41 +0200

    New function
    Added all the letters to one function instead of one per letter, in preparation for animation

--- Assets/Assignment1.cs
+++ Assets/Assignment1.cs
@@ -21,57 +21,34 @@ public class Assignment1 : ProcessingLite.GP21
     void Update()
     {
         Background(0);
-        LetterV();
-        LetterI();
-        LetterK();
-        LetterT();
-        LetterO();
-        LetterR();
+        Viktor();
 
     }
 
-    void LetterV()
+    void Viktor()
     {
+
         //V
         Line(2.5f, 7, 4, 3);
         Line(5.5f, 7, 4, 3);
-    }
 
-
-    void LetterI()
-    {
         //I
         Line(6.5f, 7, 6.5f, 3);
-    }
 
-
-    void LetterK()
-    {
         //K
         Line(8, 7, 8, 3);
         Line(8, 5, 10, 3);
         Line(10, 7, 8, 5);
-    }
 
-
-    void LetterT()
-    {
         //T
         Line(12.5f, 7, 12.5f, 3);
         Line(11, 7, 14, 7);
-    }
 
-
-    void LetterO()
-    {
         //O
         Stroke(255, 255, 255);
         Ellipse(x, y, x2, y2);
-    }
 
-
-    void LetterR()
-    {
+        //R
         Stroke(255, 255, 255);
         Circle(20, 6, 2);
         Rect(19, 7, 20, 5);
@@ -80,6 +57,6 @@ public class Assignment1 : ProcessingLite.GP21
         Line(20, 6.95f, 20, 5.05f);
         Stroke(255, 255, 255);
         Line(19, 5, 21, 3);
-
     }
+
 }
